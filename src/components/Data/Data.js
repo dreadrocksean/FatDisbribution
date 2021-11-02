cf120138d37fdbf826575e10d261bf31958006f9
Adrian <adrian@bartholomusic.com>
2021-11-01 22:02:24 -0500

    Fix dependency on same col calcs

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -14,66 +14,126 @@ const ratios = {
 const colAmount = 10;
 const staticColAmount = 2;
 
-const generalFunc = (colsRemaining, nextFunc, acc) => {
-  const currResult = acc
-    ? [...acc, nextFunc(acc[acc.length - 1])]
-    : [nextFunc()];
-  return colsRemaining
-    ? generalFunc(colsRemaining - 1, nextFunc, currResult)
-    : currResult;
+// const generalFunc = (colsRemaining, func, acc) => {
+//   const currResult = acc
+//     ? [...acc, func(acc[acc.length - 1])]
+//     : [func()];
+//   return colsRemaining
+//     ? generalFunc(colsRemaining - 1, func, currResult)
+//     : currResult;
+// };
+
+const bodyWeightCalc = (thoseColumns, thisColumn) => {
+  if (thisColumn.bodyWeight) return {res: thisColumn.bodyWeight};
+  const prevVal = thoseColumns[thoseColumns.length - 1].bodyWeight;
+  return typeof prevVal === 'undefined'
+    ? {res: leanMass + weightIncrement}
+    : {res: prevVal + weightIncrement};
 };
+
+const totalFatCalc = (thoseColumns, thisColumn) => {
+  if (thisColumn.totalFat) return {res: thisColumn.totalFat};
+  if (typeof thisColumn.bodyWeight === 'undefined')
+    return {insufficientData: true};
+  const result =
+    ((thisColumn.bodyWeight - leanMass) * 100) / thisColumn.bodyWeight;
+  return {res: `${result.toFixed(1)}%`};
+};
+
+const extemitiesCalc = val => ({res: 0});
+// val ? val + weightIncrement : leanMass + weightIncrement;
+
+const stomachCalc = val => ({res: 0});
+// val ? val + weightIncrement : leanMass + weightIncrement;
+
+const hipsCalc = val => ({res: 0});
+// val ? val + weightIncrement : leanMass + weightIncrement;
+
+const thighsCalc = val => ({res: 0});
+// val ? val + weightIncrement : leanMass + weightIncrement;
+
+const colData = [
+  {order: 0, key: 'header', name: '', init: 'Lean Body Weight', func: () => {}},
+  {order: 2, key: 'totalFat', name: 'Total Fat %', init: 0, func: totalFatCalc},
+  {
+    order: 1,
+    key: 'bodyWeight',
+    name: 'Body Weight',
+    init: leanMass,
+    func: bodyWeightCalc,
+  },
+  {
+    order: 3,
+    key: 'fatDistribution',
+    name: 'Fat Distribution',
+    init: '',
+    func: () => [],
+  },
+  {
+    order: 4,
+    key: 'extemities',
+    name: 'Extemities',
+    init: 0,
+    func: extemitiesCalc,
+  },
+  {order: 5, key: 'stomach', name: 'Stomach', init: 0, func: stomachCalc},
+  {order: 6, key: 'hips', name: 'Hips', init: 0, func: hipsCalc},
+  {order: 7, key: 'thighs', name: 'Thighs', init: 0, func: thighsCalc},
+];
 
 const Data = () => {
   const storeData = useDataValue();
   const setStoreData = useSetData();
 
-  const nextBodyWeight = val =>
-    val ? val + weightIncrement : leanMass + weightIncrement;
-
-  const nextTotalFat = val => (val ? leanMass / val / 100 : 0);
-
-  const nextExtemities = val =>
-    val ? val + weightIncrement : leanMass + weightIncrement;
-
-  const nextStomach = val =>
-    val ? val + weightIncrement : leanMass + weightIncrement;
-
-  const nextHips = val =>
-    val ? val + weightIncrement : leanMass + weightIncrement;
-
-  const nextThighs = val =>
-    val ? val + weightIncrement : leanMass + weightIncrement;
-
-  const rowData = [
-    {name: '', init: 'Lean Body Weight', func: () => []},
-    {name: 'Body Weight', init: leanMass, func: nextBodyWeight},
-    {name: 'Total Fat %', init: 0, func: nextTotalFat},
-    {name: 'Fat Distribution', init: '', func: () => []},
-    {name: 'Extemities', init: 0, func: nextExtemities},
-    {name: 'Stomach', init: 0, func: nextStomach},
-    {name: 'Hips', init: 0, func: nextHips},
-    {name: 'Thighs', init: 0, func: nextThighs},
-  ];
-
-  // ['Body Weight', leanMass]
-  const rows = useCallback(
-    rowData.map(row => [
-      ...[row.name, row.init],
-      ...generalFunc(colAmount - staticColAmount, row.func),
-    ]),
-    [colAmount, staticColAmount]
+  const cols = useCallback(
+    () =>
+      [...Array(10)].reduce((outerAcc, _, i) => {
+        const col = recursiveFunc(outerAcc, i);
+        return [...outerAcc, col];
+      }, []),
+    []
   );
 
-  const getRow = row =>
-    [...Array(colAmount)].map((_, i) => {
-      // if (i > 2) {
-      //   return {value: row.func?.(i)};
-      // }
-      return {value: row[i]};
-    });
+  const recursiveFunc = (outerAcc, col, prevAcc) => {
+    let insufficientFlags = [];
+    const data = colData.reduce((acc, row) => {
+      switch (col) {
+        case 0:
+          return {...acc, [row.key]: row.name};
+        case 1:
+          return {...acc, [row.key]: row.init};
+        default: {
+          const result = row.func?.(outerAcc, acc);
+          if (result?.insufficientData) {
+            insufficientFlags = [...insufficientFlags, row.key];
+            return acc;
+          }
+          insufficientFlags = insufficientFlags.filter(v => v !== row.key);
+          return {...acc, [row.key]: result?.res};
+        }
+      }
+    }, prevAcc ?? {});
+    return insufficientFlags.length ? recursiveFunc(outerAcc, col, data) : data;
+  };
 
-  const newData = useCallback(() => rows.map(row => getRow(row)), [rows]);
-  useEffect(() => setStoreData(newData()), [setStoreData, newData]);
+  const colsToRows = useCallback(
+    columns =>
+      [...(Array(Object.values(columns?.[0])?.length) ?? [])].map((_, i) =>
+        columns.reduce((acc, col) => {
+          const sortedKeys = [...colData]
+            .slice(0)
+            .sort((a, b) => a.order - b.order)
+            .map(v => v.key);
+          return [...acc, {value: col[sortedKeys[i]]}];
+        }, [])
+      ),
+    []
+  );
+
+  const newData = useCallback(() => colsToRows(cols()), [cols, colsToRows]);
+  useEffect(() => {
+    setStoreData(newData());
+  }, [setStoreData, newData]);
 
   const handleClick = () => setStoreData(newData());
   const handleChange = e => console.log(e);
